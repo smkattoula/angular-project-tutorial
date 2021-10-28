fbec21765604f8f3ef05b77c9f1df66782408c0a
Shaker Kattoula <smkattoula@gmail.com>
2021-10-27 18:31:22 -0700

    project 1 - generate warning-alert and success-message components, style and display them  in the application root

--- angular-projects/angular-project-tutorial/src/app/app.module.ts
+++ angular-projects/angular-project-tutorial/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
+import { SuccessMessageComponent } from './components/success-message/success-message.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessMessageComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
